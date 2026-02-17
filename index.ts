2ffdc17de4e5222e4aaab90a36d4d6911af6027a
“RitulV” <ritulcodes@gmail.com>
2026-02-17 23:33:53 +0530

    add cors

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 import searchRouter from "./routes/search";
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/search", searchRouter);
 
